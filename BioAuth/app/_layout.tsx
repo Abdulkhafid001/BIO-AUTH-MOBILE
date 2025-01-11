51c0bfc0db54e0fab38a275dfd9b2ed0580ebd1d
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2025-01-11 13:05:02 +0100

    chore:adding tensorflow to app

--- BioAuth/app/_layout.tsx
+++ BioAuth/app/_layout.tsx
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { AuthProvider } from "./auth/authContext";
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
+    <>
       <Stack>
-        <Stack.Screen name="tabs" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
+        <Stack.Screen name="tabs" options={{ headerShown: false }} />
+        <Stack.Screen name="myscreen" />
       </Stack>
       <StatusBar style="light" />
-    </AuthProvider>
-=======
-import { Stack } from 'expo-router';
-
-export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="tabs" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
->>>>>>> parent of 7f3147b (feature: add app logo)
+    </>
   );
 }
--- BioAuth/babel.config.js
+++ BioAuth/babel.config.js
@@ -1,15 +1,28 @@
-module.exports = {
-  presets: ["module:@react-native/babel-preset", "babel-preset-expo"],
-//   plugins: [
-//     "nativewind/babel",
+// module.exports = {
+//   presets: [
 //     [
-//       "module-resolver",
+//       "@babel/preset-react",
 //       {
-//         root: ["./src"],
-//         alias: {
-//           "@": "./src",
-//         },
+//         runtime: "automatic",
 //       },
 //     ],
 //   ],
-};
+  // plugins: [
+  //   "nativewind/babel",
+  //   [
+  //     "module-resolver",
+  //     {
+  //       root: ["./src"],
+  //       alias: {
+  //         "@": "./src",
+  //       },
+  //     },
+  //   ],
+  // ],
+// };
+module.exports = function(api){
+  api.cache(true);
+  return{
+    presets: ["babel-preset-expo"]
+  }
+}
